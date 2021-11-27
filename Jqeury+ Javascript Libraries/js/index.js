68d2afb6ba3008100645b29482b5cccb344360e3
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-26 21:36:51 -0800

    Update jQuery demo

--- Jqeury+ Javascript Libraries/js/index.js
+++ Jqeury+ Javascript Libraries/js/index.js
@@ -1,3 +1,23 @@
 'use strict';
 
 /* */
+/* use jquery to create a function with an onpage load */
+
+$(() =>{
+/* select all the p tags */
+let p = jQuery('p');
+
+/* set the p  text to be welcome to the page */
+p.text = 'welcome to the page';
+
+$('p').text = "Welcome page";
+
+/* Use jquery to create a new tag similar to document.createElement */
+
+let newTag = $('<p> Welcome page </p>');
+
+
+/* Select the main tag using jquery and append the child the new tag you created. */
+
+$('main').appendChild(newTag);
+})
