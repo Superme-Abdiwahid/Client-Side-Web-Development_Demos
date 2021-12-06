71d9a0d98373d964e0e710c98a6f10f1512b279e
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-12-05 18:49:27 -0800

    Update React Demo

---
+++ SecondCode.js
@@ -0,0 +1,12 @@
+import React from "react";
+import image from './img/abdiwahid.png';
+
+
+
+export default class SecondCode extends React.Component{
+render(){
+    return(<img class="Plots" src={image}/>)
+}
+
+
+}
