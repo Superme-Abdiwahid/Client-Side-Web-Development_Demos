37f6b41a43cfbd6f97c5511f58e9a5c2ae0f3d46
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-12-05 18:24:28 -0800

    Update React Demo

---
+++ Advanced React/advanced_react_demo/src/Rstudio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from './img/Practice.png';
+import './style.css';
+
+export default class Rstudio extends React.Component{
+    render(){
+        return(
+            <img className="Pratice_Markdown" src={image}/>
+
+        )
+    }
+}
