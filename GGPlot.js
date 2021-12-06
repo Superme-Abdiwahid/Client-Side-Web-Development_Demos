853622b8266986c7832f194fd5f05f5c42192b81
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-12-05 18:49:45 -0800

    Update React Demo

---
+++ GGPlot.js
@@ -0,0 +1,13 @@
+import React from "react";
+import img from "./img/Plots.png";
+import './style.css'
+
+
+export default class GGPlot extends React.Component{
+    render(){
+        return(
+<img class="Plots" src={img}/>
+        )
+    }
+
+}
