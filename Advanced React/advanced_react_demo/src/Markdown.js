4d5dd71277f9501933ac62b0c628433d75b2a2ba
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-12-05 18:18:26 -0800

    Update React Demo

---
+++ Advanced React/advanced_react_demo/src/Markdown.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from './img/markdown.png';
+import './style.css';
+
+export default class Markdown extends React.Component{
+    render(){
+        return(
+<img className="Second_Page" src={image} />        )
+    }
+}
