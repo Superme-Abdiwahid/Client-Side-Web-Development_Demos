b594e8b3fc42ed41f625a3919ff59e2d860f329c
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-12-05 18:26:16 -0800

    Export Image

---
+++ Advanced React/advanced_react_demo/src/ImagePlot.js
@@ -0,0 +1,13 @@
+import React from "react"
+import image from './img/readFile.png'; 
+
+
+
+export default class ImagePlot extends React.Component{
+    render(){
+        return(
+    <img  class="Plot"  src={image}/>
+
+        )
+    }
+}
